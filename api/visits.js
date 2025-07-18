d763756459a80ffd17013e755b8cc3495f021abe
Rayen070 <haddajirayen052@gmail.com>
2025-07-18 19:55:59 +0200

    api/visits.js

---
+++ api/visits.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+  const universeId = '6242919762'; // Universe ID Motorcycle Racing
+  const url = `https://games.roblox.com/v1/games?universeIds=${universeId}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const visits = data.data[0].visits;
+
+    res.status(200).json({ visits });
+  } catch (err) {
+    res.status(500).json({ error: 'Errore nel recupero delle visite.' });
+  }
+}
