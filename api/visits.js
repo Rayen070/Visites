347b0fd2daa38c7915a2e46920db5970e7a73333
Rayen070 <haddajirayen052@gmail.com>
2025-07-19 11:35:04 +0200

    Update visits.js

--- api/visits.js
+++ api/visits.js
@@ -1,11 +1,16 @@
 export default async function handler(req, res) {
+  // Imposta header CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
   const universeId = '7332812970'; // Universe ID Motorcycle Racing
   const url = `https://games.roblox.com/v1/games?universeIds=${universeId}`;
 
   try {
     const response = await fetch(url);
     const data = await response.json();
-    const visits = data.data[0].visits;
+    const visits = data.data?.[0]?.visits || 0; // fallback a 0 se undefined
 
     res.status(200).json({ visits });
   } catch (err) {
